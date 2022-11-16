aa00b332f1589c0b516a0953ef18862dcf3e480c
Teng Gao <tgaoteng@gmail.com>
2022-11-16 15:13:52 -0500

    Update test_functions.R

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -1,7 +1,7 @@
 library(scistreer)
 
 test_that("Small input works", {
-    # tree = run_scistree(P_small, verbose = F)
-    # expect_equal(round(tree$likelihood, 3), -1606.497)
+    tree = run_scistree(P_small, verbose = F)
+    expect_equal(round(tree$likelihood, 3), -1606.497)
     expect_equal(1,1)
 })
