480574cddf7cbab45943a9bf7a262753b56c1534
teng-gao <tgaoteng@gmail.com>
2022-11-21 12:40:42 -0500

    exporting additional functions

--- R/data.R
+++ R/data.R
@@ -6,6 +6,18 @@
 #' 
 "P_small"
 
-#' Smaller example tree built from P_small
+#' Smaller example tree built from P_small 
 #' 
 "tree_small"
+
+#' Smaller example annotated tree built from P_small
+#' 
+"gtree_small"
+
+#' Example tree built using UPGMA from P_small
+#' 
+"tree_upgma"
+
+#' Mutation placements calculated from tree_small and P_small
+#'
+"mut_nodes_small"
